63e5bd13fec7298d33a08efb785c4ee91c9457f2
othman853 <anuar.yasser@gmail.com>
2017-02-16 17:17:36 -0200

    Serving some view

--- app/routes/Hello.rb
+++ app/routes/Hello.rb
@@ -1,10 +1,15 @@
 require 'sinatra'
+require_relative './BaseRoute'
 require_relative '../models/Messages'
 
-class Hello < Sinatra::Base
+class Hello < BaseRoute
 
   get '/dumbass' do
     Messages.new.dumbass
+  end
+
+  get '/buttons' do
+    erb :buttons, :format => :html5
   end
 
 end
